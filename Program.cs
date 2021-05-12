1466644fe5770294d877ef7dba193e1108f9db5c
Craft <william.craft@accenture.com>
2021-05-12 12:05:21 -0400

    added comments for clarity

--- Program.cs
+++ Program.cs
@@ -6,17 +6,22 @@ namespace C__Practice
     {
         static void Main(string[] args)
         {
+            // Initialize Variables
             string firstName;
             string lastName;
 
+            // User is prompted for First and last name.
             Console.WriteLine("Hello World. My first C# program!");
             Console.WriteLine("Please type in your first name:");
             firstName = Console.ReadLine();
             Console.WriteLine("Okay! Now, please type in your last name:");
             lastName = Console.ReadLine();
 
+            // Console will then respond with the first and last names in a complete sentence.
             Console.WriteLine("Your full name is " + firstName + " " + lastName + ". It's nice to meet you!");
 
+            // Program will then ask for two numbers which are bing converted from String to Int.
+            // Line 29 adds the two converted numbers and 30 is logging the sum. 
             Console.WriteLine("Now, we're going to do math. Enter a number:");
             int firstNumber = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Now enter a second number:");
