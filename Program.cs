e9f999f38eb93739e91009af14b822e0b0112c82
Craft <william.craft@accenture.com>
2021-05-12 11:35:35 -0400

    users can now enter a number and the program will do basic addition.

--- Program.cs
+++ Program.cs
@@ -6,15 +6,25 @@ namespace C__Practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World. My first C# program!");
             string firstName;
             string lastName;
+
+            Console.WriteLine("Hello World. My first C# program!");
             Console.WriteLine("Please type in your first name:");
             firstName = Console.ReadLine();
             Console.WriteLine("Okay! Now, please type in your last name:");
             lastName = Console.ReadLine();
 
             Console.WriteLine("Your full name is " + firstName + " " + lastName + ". It's nice to meet you!");
+
+            Console.WriteLine("Now, we're going to do math. Enter a number:");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Now enter a second number:");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            int sum = firstNumber + secondNumber;
+            Console.WriteLine("The sum of your two numbers is: " + sum);
+
+
         }
     }
 }
