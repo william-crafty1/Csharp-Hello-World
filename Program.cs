a8c00a7bd8e4288003ceefbe7c45f8e2c798facd
Craft <william.craft@accenture.com>
2021-05-12 11:22:34 -0400

    first C# program, simple Hello World and name input

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace C__Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World. My first C# program!");
+            string firstName;
+            string lastName;
+            Console.WriteLine("Please type in your first name:");
+            firstName = Console.ReadLine();
+            Console.WriteLine("Okay! Now, please type in your last name:");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("Your full name is " + firstName + " " + lastName + ". It's nice to meet you!");
+        }
+    }
+}
